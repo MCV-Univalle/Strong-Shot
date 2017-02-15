e50f4afdbfa56f618de9f2a1b6390e847149cd6e
Salazarjuan <salazar.juan@correounivalle.edu.co>
2017-02-15 18:17:39 -0500

    TG-15 #ready-for-test

---
+++ StrongShot/Assets/Scripts/ColorShooter/moveTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTarget : MonoBehaviour {
+
+	public float spinSpeed = 180.0f;
+	public float moveSpeed = 0.1f;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate (Vector3.up*spinSpeed*Time.deltaTime);	
+		//gameObject.transform.Rotate (Vector3.back*spinSpeed*Time.deltaTime);
+		//gameObject.transform.Rotate (Vector3.left*spinSpeed*Time.deltaTime);
+		gameObject.transform.Translate(Vector3.back*Mathf.Cos(Time.timeSinceLevelLoad)*moveSpeed);
+	}
+}
