6ca56599a26625f7353118f20360e3c3eaaa636f
tsubasa1218 <juan.suaza@correounivalle.edu.co>
2017-02-21 16:51:10 -0500

    Merged project
    Merge MixedWords into Strong-shot folder

---
+++ StrongShot/Assets/Scripts/MixedWords/ButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+
+    public Button button;
+    public Text letter;
+    public string letterSelected;
+    private GameController gameController;
+    public Text CurrentWord;
+    private string message = "";
+
+    public void GetLetter()
+    {
+		
+		letterSelected = letter.text;
+		message = gameController.CurrentWord (letterSelected, ref button);
+		button.interactable = false;
+		CurrentWord.text = message;
+    }
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			message = "";
+			CurrentWord.text = message;
+		}
+	}
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+}
