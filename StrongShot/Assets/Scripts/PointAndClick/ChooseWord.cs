4ce8dd9262e1147b6f6433fe4871ee53e831d225
camilo1090 <camilo.ruiz.casanova@correounivalle.edu.co>
2017-02-28 14:41:42 -0500

    TG-19 #ready-for-test

---
+++ StrongShot/Assets/Scripts/PointAndClick/ChooseWord.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseWord : MonoBehaviour
+{
+    public void Choose()
+    {
+        string word = gameObject.name;
+
+        GameManagerPointAndClick.gmpac.VerifySelection(word);
+    }
+}
